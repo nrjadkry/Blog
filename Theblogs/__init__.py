ac6cddf3eb08a6a147cae853fbfeaa404aca6eb4
niraj <nrjadkry@gmail.com>
2020-06-28 19:29:31 +0545

    UserProfile is made

---
+++ Theblogs/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'Theblogs.apps.TheblogsConfig'
--- Theblogs/apps.py
+++ Theblogs/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class TheblogsConfig(AppConfig):
     name = 'Theblogs'
+
+    def ready(self):
+    	import Theblogs.signals
